e3acad5287250576604bf1e6b29477edd1ae5715
huahua <ma.xiuyuan42@gmail.com>
2025-07-12 20:42:33 +0800

    up

--- P1319.cpp
+++ P1319.cpp
@@ -1,3 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int A[100];
+int n, d, t;
+int flag = 1;
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n * n;)
+    {
+        cin >> d;
+        t = d;
+        if (flag)
+            flag = 0;
+        else
+            flag = 1;
+        for (; d > 0; d--)
+        {
+            i++;
+            if (i % n == 0)
+            {
+                cout << flag << endl;
+            }
+            else
+            {
+                cout << flag;
+            }
+        }
+    }
+    return 0;
+}
