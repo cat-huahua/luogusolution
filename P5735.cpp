b566a18a8d27b6ecd6de4a03e56eae75cba9ff81
huahua <ma.xiuyuan42@gmail.com>
2025-07-23 20:44:53 +0800

    update

--- P5735.cpp
+++ P5735.cpp
@@ -1,7 +1,17 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
-float x1,y1,x2,y2,x3,y3;
-int main(){
+double dis(double xa, double ya, double xb, double yb)
+{
+    return sqrt((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb));
+}
+int main()
+{
+    double x1, y1, x2, y2, x3, y3,d = 0;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+    d += dis(x1,y1,x2,y2);
+    d += dis(x2,y2,x3,y3);
+    d += dis(x3,y3,x1,y1);
+    printf("%0.2lf",d);
     return 0;
 }
