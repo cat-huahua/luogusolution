9aafd65c0c466b8fa641e4df8621ebd3eb405813
huahua <ma.xiuyuan42@gmail.com>
2025-07-24 17:48:33 +0800

    update

--- P5736.cpp
+++ P5736.cpp
@@ -1,3 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int l;
+int is_prime(int din)
+{
+    for (int i = 2; i <= sqrt(din); i++)
+    {
+        if (din % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int l, ip;
+    cin >> l;
+    for (int j = 0; j < l; j++)
+    {
+        cin >> ip;
+        if (is_prime(ip))
+        {
+            cout << ip << " ";
+        }
+    }
+    return 0;
+}
