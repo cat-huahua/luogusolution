7cbf80568db4e775d4c5f6a26c43ca0252cb61f8
huahua <ma.xiuyuan42@gmail.com>
2025-08-02 21:42:37 +0800

    update

--- P1047.cpp
+++ P1047.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-bool t[10000];
-int ts = 0, ls = 0, h = 0, e = 0, tl = 0;
+bool t[10001];
+
 int ct(int head, int end)
 {
     for (; head <= end; head++)
     {
-        t[head] == false;
+        t[head] = false;
     }
     return 0;
 }
+
 int main()
 {
+    int ts = 0, ls = 0, h = 0, e = 0, tl = 0;
+
     cin >> ts >> ls;
-    memset(t, 1, ts);
+    memset(t, 1, ts+1);
     for (; ls > 0; ls--)
     {
         cin >> h >> e;
         ct(h, e);
     }
-    for (int i = 0; i < 10000; i++)
+
+    for (int i = 0; i <= ts; i++)
     {
-        if (t[i] = true)
+        if (t[i] == true)
         {
             tl += 1;
         }
