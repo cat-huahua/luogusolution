f5b628f774300290aed7ed91d5c435e8f1f906a2
huahua <ma.xiuyuan42@gmail.com>
2025-08-09 12:31:56 +0800

    update

---
+++ P1914.cpp
@@ -0,0 +1,7 @@
+#include<algorithm>
+#include<iostream>
+#include<string>
+using namespace std;
+string s;
+int n;
+int main()
--- P5733.cpp
+++ P5733.cpp
@@ -1,13 +1,15 @@
-#include<algorithm>
-#include<iostream>
-#include<cctype>
-#include<string>
+#include <algorithm>
+#include <iostream>
+#include <cctype>
+#include <string>
 using namespace std;
 string s;
-int main(){
-    cin>>s;
-transform(s.begin(), s.end(), s.begin(), 
-    [](unsigned char c){ return toupper(c); });
-    cout<<s;
+int main()
+{
+    cin >> s;
+    transform(s.begin(), s.end(), s.begin(),
+              [](unsigned char c)
+              { return toupper(c); });
+    cout << s;
     return 0;
 }
