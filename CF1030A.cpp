ce6cd9bb59b91b73ef6dff0112d11a94a0dc3ac1
huahua <ma.xiuyuan42@gmail.com>
2025-08-21 17:27:41 +0800

    update

--- CF1030A.cpp
+++ CF1030A.cpp
@@ -1,6 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int n, R[100];
-int main(){
-    
+int n;
+bool t;
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> t;
+        if (t == 1)
+        {
+            cout << "HARD";
+            return 0;
+        }
+    }
+    cout << "EASY";
+    return 0;
 }
