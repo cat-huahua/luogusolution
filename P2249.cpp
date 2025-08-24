14f87b742c798d19e46456547e2c1e9e3da10760
huahua <ma.xiuyuan42@gmail.com>
2025-08-24 20:14:34 +0800

    update

--- P2249.cpp
+++ P2249.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
-int n, m, A[1000000000];
+int n, m, A[1000000000],t;
 int main()
 {
     cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
         cin >> A[i];
+    }
+    for (; i < count; i++)
+    {
+        /* code */
     }
     
     return 0;
