611b2844f0cf11c631fe59ed4d8021046ef80361
huahua <ma.xiuyuan42@gmail.com>
2025-08-25 20:27:40 +0800

    update

--- P2249.cpp
+++ P2249.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 using namespace std;
-int n, m, A[1000000000], t, a = 0, z = 0;
+int n, m, A[1000000000];
 
-int bfind(int c){
-    int b=0,e=n-1,i;
-    while (b != e)
+int bfind(int c)
+{
+    int b = 0, e = n, i, r;
+    r = -2;
+    while (b < e)
     {
-        i = b+e/2
-        if (A[i] == )
+        i = (b + e) / 2;
+
+        if (A[i] == c)
         {
-            /* code */
+            r = i;
         }
-        
+        if (A[i] >= c)
+        {
+            e = i;
+        }
+        else
+        {
+            b = i + 1;
+        }
     }
-    
+    return r;
 }
+
 int main()
 {
+    int t;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
         cin >> A[i];
     }
-    for (int i; i < m; i++)
+    for (int i = 0; i < m; i++)
     {
         cin >> t;
-        cout<< bfind(t)
+        cout << bfind(t) + 1 << " ";
     }
 
     return 0;
