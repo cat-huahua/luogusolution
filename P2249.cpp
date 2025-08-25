130076a1636b162c4db3f8fc53b4167276dda590
huahua <ma.xiuyuan42@gmail.com>
2025-08-25 20:37:15 +0800

    update

--- P2249.cpp
+++ P2249.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
 using namespace std;
-int n, m, A[1000000000];
+int n, m, A[1000000];
 
 int bfind(int c)
 {
-    int b = 0, e = n, i, r;
-    r = -2;
-    while (b < e)
-    {
-        i = (b + e) / 2;
 
-        if (A[i] == c)
-        {
-            r = i;
-        }
-        if (A[i] >= c)
-        {
-            e = i;
-        }
-        else
-        {
-            b = i + 1;
-        }
-    }
-    return r;
 }
 
 int main()
