0127eaab90f053d9cabca38deb055f31883c279a
huahua <ma.xiuyuan42@gmail.com>
2025-09-04 13:12:04 +0800

    update

--- P1789.cpp
+++ P1789.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
-int n, m, k, x, y, arr[1000];
+int n, m, k, x, y, o,p, arr[10000];
 int main()
 {
     cin >> n >> m >> k;
-    for (int i = 0; i < k; i++)
+    for (int i = 0; i < m; i++)
     {
         cin >> x >> y;
-        
+        arr[(x-1)*10+(y-1)] = 1;
     }
+    for (int i = 0; i < k; i++)
+    {
+        cin >> o >> p;
+        arr[(o-1)*10+(p-1)] = 1;
+    }
+    
     return 0;
 }
