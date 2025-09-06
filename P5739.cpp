53baa83b02fb14938162bf83a8fc31309b50dc1f
huahua <ma.xiuyuan42@gmail.com>
2025-09-06 08:39:21 +0800

    upload

--- P5739.cpp
+++ P5739.cpp
@@ -1,17 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int a;
-int fact(int n){
+int fact(int n)
+{
     int r = 1;
-    for (int i = 0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        r*(i+1);
+        r *= (i + 1);
     }
     return r;
 }
-int main(){
-    cin>>a;
-    int r = fact(a);
-    cout<<r;
+int main()
+{
+    cin >> a;
+    int ans = fact(a);
+    cout << ans << endl;
     return 0;
 }
