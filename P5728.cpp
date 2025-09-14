ea4db5892faf2eb0e54ebbc23c03660c3e74ff6f
cat_huahua <ma.xiuyuan42@gmail.com>
2025-09-14 17:52:17 +0800

    UP

--- P5728.cpp
+++ P5728.cpp
@@ -22,9 +22,9 @@ int main()
         Array[i][3] = Array[i][0] + Array[i][1] + Array[i][2];
     }
     Cnt = 0;
-    for (int i = 0; i < N - 1; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < N - i; j++)
+        for (int j = i + 1; j < N; j++)
         {
             if (cmp(Array[i][3], Array[j][3], 10) && cmp(Array[i][0], Array[j][0], 5) && cmp(Array[i][1], Array[j][1], 5) && cmp(Array[i][2], Array[j][2], 5))
             {
