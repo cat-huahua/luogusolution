e425777088017d540e8cd5f8eccaf885be036130
huahua <ma.xiuyuan42@gmail.com>
2025-09-14 17:02:15 +0800

    update

--- P5728.cpp
+++ P5728.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
-int Array[1000];
+int Array[1000][4], N, Cnt;
+int cmp(int a, int b, int c)
+{
+    if (abs(a - b) < c)
+    {
+        return 1;
+    }
+    return 0;
+}
 int main()
 {
-
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> Array[i][j];
+        }
+        Array[i][3] = Array[i][0] + Array[i][1] + Array[i][2];
+    }
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = 0; j < N - i; j++)
+        {
+            if (cmp(Array[i][3], Array[j][3], 10) && cmp(Array[i][0], Array[j][0], 5) && cmp(Array[i][1], Array[j][1], 5) && cmp(Array[i][2], Array[j][2], 5))
+            {
+                Cnt++;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    cout << Cnt << endl;
     return 0;
 }
