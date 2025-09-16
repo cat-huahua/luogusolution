c6f51c441d1fa582bd09f15679f1a5b724a326a8
cat_huahua <ma.xiuyuan42@gmail.com>
2025-09-16 19:40:06 +0800

    update

--- P5732.cpp
+++ P5732.cpp
@@ -4,13 +4,13 @@ int n, Array[20][20];
 int main()
 {
     cin >> n;
-    cout << 1 << endl;
-    Array[0][0] = 1;
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i <= n; i++)
     {
-        for (int j = 0; j < i; j++)
+        Array[i][0] = 1;
+        cout << Array[i][0] << " ";
+        for (int j = 1; j <= i; j++)
         {
-            Array[i][j] = Array[i - 1][j] + Array[i][j - 1];
+            Array[i][j] = Array[i - 1][j] + Array[i - 1][j - 1];
             cout << Array[i][j] << " ";
         }
         cout << endl;
