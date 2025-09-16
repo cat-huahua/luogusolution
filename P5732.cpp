523c463d19639969df5fe7d13f039d982b17395b
cat_huahua <ma.xiuyuan42@gmail.com>
2025-09-16 13:14:39 +0800

    update

--- P5732.cpp
+++ P5732.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
-int n,array[20][20];
+int n, Array[20][20];
 int main()
 {
     cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            
+        }
+    }
     
     return 0;
 }
