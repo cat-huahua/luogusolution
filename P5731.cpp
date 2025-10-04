cd164c191f17e7bec6c27b8e0591a8cc1ff4b569
cat_huahua <ma.xiuyuan42@gmail.com>
2025-10-04 17:31:09 +0800

    update

--- P5731.cpp
+++ P5731.cpp
@@ -3,7 +3,7 @@
 
 ## 题目描述
 
-给出一个不大于 $9$ 的正整数 $n$，输出 $n\times n$ 
+给出一个不大于 $9$ 的正整数 $n$，输出 $n\times n$
 的蛇形方阵。
 
 从左上角填上 $1$ 开始，顺时针方向依次填入数字，如同样例所示。注意每个数字有都会占用 $3$ 个字符，前面使用空格补齐。
@@ -27,7 +27,7 @@
 ### 输出 #1
 
 ```
-1  2  3  4
+  1  2  3  4
  12 13 14  5
  11 16 15  6
  10  9  8  7
@@ -37,3 +37,35 @@
 
 数据保证，$1 \leq n \leq 9$。
 */
+#include <iostream>
+using namespace std;
+int n, A[9][9], d = 0;
+int Di[] = {0, 1, 0, -1};
+int Dj[] = {1, 0, -1, 0};
+
+bool iswall(int i, int j)
+{
+    if (i < 0 || j < 0 || i >= n || j >= n || A[i][j] != 0)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    cin >> n;
+    int i = 0, j = 0,x,y;
+    for (int k = 1; k <= n * n; k++)
+    {
+        A[i][j] = k;
+        x = i + Di[d];
+        y = j + Dj[d];
+        if (iswall(x,y))
+        {
+            /* code */
+        }
+        
+    }
+
+    return 0;
+}
