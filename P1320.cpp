f26557ba8ff9c84fda366f57556cb83a3068a91b
cat_huahua <ma.xiuyuan42@gmail.com>
2025-10-11 10:24:38 +0800

    update

--- P1320.cpp
+++ P1320.cpp
@@ -1,12 +1,28 @@
 #include <cstdio>
-#include <string.h>
 using namespace std;
-int N, Y = 0;
-char A[200][200];
+int N = 0;
+char A[200][200], Sta = 0, C = 0, temp;
 int main()
 {
-    while (scanf("%s", A[0][Y] != -1), Y++)
+    while (scanf("%s", A[N]) != EOF)
     {
+        N++;
     }
-    N = strlen(A[0]);
+    printf("%d", N);
+    for (int i = 0; i < N*N; i++)
+    {
+        temp = A[i / N][i % N];
+        if (temp == Sta)
+        {
+            C++;
+        }
+        else
+        {
+            printf("%d", C);
+            Sta = temp;
+            C = 1;
+        }
+    }
+    printf("\n");
+    return 0;
 }
