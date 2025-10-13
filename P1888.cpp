73b842e2daafa6c67ca0b03f10c6d7235a1c693b
cat_huahua <ma.xiuyuan42@gmail.com>
2025-10-13 17:15:20 +0800

    update

---
+++ P1888.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <numeric>
+using namespace std;
+int A[3], g;
+
+int main()
+{
+    cin >> A[0] >> A[1] >> A[2];
+    sort(A, A + 3);
+    g = gcd(A[0], A[2]);
+    printf("%d/%d\n", A[2] / g, A[0] / g);
+    return 0;
+}
