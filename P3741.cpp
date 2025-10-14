01ccf962720f2b2136ce620f5697aff0e1f19919
cat_huahua <ma.xiuyuan42@gmail.com>
2025-10-14 09:58:20 +0800

    update

--- P3741.cpp
+++ P3741.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
+string s;
+int n, cnt = 0, c = 1;
+int main()
+{
+    cin >> n;
+    cin >> s;
 
-int main() {
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'v' && s[i + 1] == 'k')
+        {
+            cnt++;
+        }
+        if (s[i] == 'v' && s[i + 1] == 'v', c > 0)
+        {   cnt++;
+            c--;
+        }
+    }
+
+    cout << cnt << endl;
 
     return 0;
 }
