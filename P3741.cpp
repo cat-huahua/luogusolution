1df6bab73df8b98a4c2d49d8c54be9577980e7ee
cat_huahua <ma.xiuyuan42@gmail.com>
2025-10-15 10:20:43 +0800

    update

--- P3741.cpp
+++ P3741.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
 using namespace std;
 string s;
-int n, cnt = 0, c = 1;
+int n, cnt = 0;
+
 int main()
 {
     cin >> n;
     cin >> s;
 
-    for (int i = 0; i < n; i++)
+    // Count initial VK substrings
+    for (int i = 0; i < n - 1; i++)
     {
-        if (s[i] == 'v' && s[i + 1] == 'k')
+        if (s[i] == 'V' && s[i + 1] == 'K')
         {
             cnt++;
         }
-        if (s[i] == 'v' && s[i + 1] == 'v', c > 0)
-        {   cnt++;
-            c--;
-        }
     }
+    for (int i = 0; i < n; i++)
+    {
+        char original = s[i];
+        for (char c : {'V', 'K'})
+        {
+            if (c == original) continue;
+            s[i] = c;
+            int tempCnt = 0;
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (s[j] == 'V' && s[j + 1] == 'K')
+                {
+                    tempCnt++;
+                }
+            }
+            if (tempCnt > cnt)
+            {
+                cnt = tempCnt;
+            }
+        }
+        s[i] = original;
+    }
+
     cout << cnt << endl;
     return 0;
 }
