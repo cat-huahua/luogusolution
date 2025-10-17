851956723496dc2af758aa29fbd3faf077787764
cat_huahua <ma.xiuyuan42@gmail.com>
2025-10-17 16:24:43 +0800

    update

---
+++ P5729.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int A[20][20][20], w, x, h;
+int cut()
+int main()
+{
+    int q;
+    cin >> w >> x >> h >> q;
+    memset(A, 0, sizeof(A));
+    memset(A, 1, A[x][y][q]);
+
+    return 0;
+}
