1f673718b7785911c7d6503d3c38b1ce807574ca
cat_huahua <ma.xiuyuan42@gmail.com>
2025-11-02 15:54:30 +0800

    update

--- P3156.cpp
+++ P3156.cpp
@@ -1,1 +1,16 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+int a[2000000], n, m;
+int main()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    for (int i = 1; i <= m; i++)
+    {
+        int ai;
+        cin >> ai;
+        cout << a[ai] << endl;
+    }
+    return 0;
+}
