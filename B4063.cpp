79cbab404c84ba89e8347612bf1a1b7f6da03416
cat_huahua <ma.xiuyuan42@gmail.com>
2025-11-10 16:31:28 +0800

    update

--- B4063.cpp
+++ B4063.cpp
@@ -1,15 +1,15 @@
-#include <iostearm>
+#include <iostream>
 using namespace std;
 int n, i, cnt;
 int main()
 {
     cin >> n;
-    for(i = 0,int in ;i < n; i++)
+    for(int in,i = 0;i < n; i++)
     {
         cin >> in;
         if(in % 2 == 0)
             cnt++;
     }
-    cout << i - cnt << " " <<cnt:
+    cout << n - cnt << " " << cnt <<endl;
     return 0;
 }
