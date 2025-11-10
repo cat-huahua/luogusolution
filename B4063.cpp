6f7279836c93ad81c46bb9a03325be04a7288ac0
cat_huahua <ma.xiuyuan42@gmail.com>
2025-11-10 16:22:55 +0800

    update

--- B4063.cpp
+++ B4063.cpp
@@ -1,1 +1,15 @@
-
+#include <iostearm>
+using namespace std;
+int n, i, cnt;
+int main()
+{
+    cin >> n;
+    for(i = 0,int in ;i < n; i++)
+    {
+        cin >> in;
+        if(in % 2 == 0)
+            cnt++;
+    }
+    cout << i - cnt << " " <<cnt:
+    return 0;
+}
