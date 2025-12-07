17b87a6b3e257abb20f08b06fb7430cab0ad9301
cat_huahua <ma.xiuyuan42@gmail.com>
2025-12-07 10:38:43 +0800

    update

---
+++ B3865.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int N;
+int main()
+{
+    cin >> N;
+    for(int i = 0;i < N;i++)
+    {
+        for(int j = 0;j < N;j++)
+        {
+            if(j == i || j == N - i)
+                cout << '+';
+            else
+                cout << '-';
+        }
+        cout << endl;
+    }
+    return 0;
+}
